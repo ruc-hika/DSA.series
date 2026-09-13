c92a22d01408fdfe43b14916465f95bc427088af
Ruchika <ruchikanagar030@gmail.com>
2026-09-13 21:49:09 +0530

    Implement largestOverlap method in Dailychallenge1.java

---
+++ Array/Dailychallenge1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        int max = 0;
+        for(int r = -n + 1;r<n;r++){
+            for( int c = -n + 1;c<n;c++){
+                int count = 0;
+                for(int i =0;i<n;i++){
+                    for(int j =0;j<n;j++){
+                        int x = i + r;
+                        int y = j + c;
+                        if(x >=0 && x<n && y>=0 && y<n){
+                            if(img1[i][j]==1 && img2[x][y] == 1){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                max = Math.max(max, count);
+            }
+        }
+        return max;
+    }
+}
